d2f98e8469a8b6dc96b17b7b39cf67a5d7f4dbb3
Enrico <enrico.cavinato.1@studenti.unipd.it>
2022-01-08 15:46:02 +0100

    implementata think

--- lib/Bot.cpp
+++ lib/Bot.cpp
@@ -5,18 +5,43 @@
 
 */
 
+#include <random>
+#include <array>
+#include <vector>
+
 #include "../include/Bot.h"
 
 namespace chessgame
 {
     Bot::Bot() : Player() {}
-    Bot::Bot(const PieceColor& p_color) : Player(p_color) {}
-    /**
-
-        @brief: This function represents the act of thinking a move. Overloads the function Player::think() and randomize it
-        @return: array<Coordinates,2> the original and final coordinates of the piece we want to move
-    */
-    std::array<Coordinates,2>& Bot::think() {}
+    Bot::Bot(const PieceColor& p_color) : Player(p_color) 
+    {}
+    std::array<Coordinates,2>& Bot::think(Chessboard& board) 
+    {
+        // generate the coordinates of a random cell we want to inspectionate
+        int row {std::rand() % ROWS};
+        int column {std::rand() % COLUMNS};
+        // while loop visit every piece of given color and considers its possible moves
+        bool done {false};
+        while (!done)
+        {
+            // get almost random Coordinates of a piece of given color
+            Coordinates from {board.get(pieceColor,row,column,0)};
+            // get the possible moves
+            std::vector<Coordinates> possible_moves {board.get_piece(from)->getMoves(board,from)};
+            // if a possible move does not exist
+            if (possible_moves.size() == 0) 
+            {
+                // continue the loop but get() will continue to iterate the matrix from the cell where it stopped at the previous iteration
+                row = from.y;
+                column = from.x +1;
+                continue;
+            }
+            // if a possible move does exist
+            Coordinates to {possible_moves[0]};
+            return std::array<Coordinates,2> {from,to};
+        }
+    }
     Piece* Bot::getPromotionTarget() {
         char c = '0';
         if (pieceColor == WHITE) c = 'd';
