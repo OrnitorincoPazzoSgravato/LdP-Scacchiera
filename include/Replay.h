64d8e8dc206ecc6643ca8f643c954e9995dabf3d
Enrico <enrico.cavinato.1@studenti.unipd.it>
2022-01-09 15:52:27 +0100

    commenti per i metodi di replay

--- include/Replay.h
+++ include/Replay.h
@@ -28,9 +28,25 @@ namespace chessgame
     {
         Chessboard cboard;    
     public:
+        /**
+         * @brief Construct a new replay
+         * 
+         */
         Replay();
-        void print_onscreen(const std::string&);
-        void print_on_file(const std::string& ,const std::string&);
+        /**
+         * @brief print a game on screen from a log file. The function prints the seuqence of the states of the chessboard before and avter every move
+         * 
+         * @param input_file the log file we want to read
+         * 
+         */
+        void print_onscreen(const std::string& input_file);
+        /**
+         * @brief print a game on a log file from a log file. The function prints the seuqence of the states of the chessboard before and avter every move
+         * 
+         * @param input_file the log file we want to read
+         * 
+         */
+        void print_on_file(const std::string& input_file,const std::string& output_file);
     };
 }
 
