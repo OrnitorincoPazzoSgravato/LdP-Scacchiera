4c865c73f25d9ee936f33c23e5ee101075e920a1
Enrico <enrico.cavinato.1@studenti.unipd.it>
2022-01-12 21:24:16 +0100

    Created class human

---
+++ include/chessgame/Human.h
@@ -0,0 +1,55 @@
+/**
+ * @file Human.h
+ * @author Enrico Cavinato
+ * @brief This class implements a human player
+ * @version 0.1
+ * @date 2022-01-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include <array>
+#include <string>
+
+#include "Player.h"
+
+namespace chessgame
+{
+    class Human : public Player
+    {
+    private:
+        /* data */
+    public:
+        /**
+         * @brief Construct a new Human object
+         * 
+         * @param pcolor the color of this player
+         */
+        Human(const PieceColor pcolor) 
+            : Player {pcolor}
+        {}
+        /**
+         * @brief This function implement the act of thinking a move by a human player
+         * 
+         * @return std::array<Coordinates, 2> an array with the move 
+         */
+        std::array<Coordinates, 2> think()
+        {
+            std::string from, to;
+            std::cin >> from >> to;
+            return std::array<Coordinates, 2>{Coordinates(from), Coordinates(to)};
+        }
+        /**
+         * @brief Implement the decision of the piece we want to replace the paw with
+         * 
+         * @return Piece* a pointer to the piece we want to replace the paw with
+         */
+        Piece* getPromotionTarget();        
+
+    }; 
+}
+#endif
