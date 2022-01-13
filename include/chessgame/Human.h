cec5300ce1a823331e27ae215949c5ebc8af5d8b
HeliosVelite <heliosvelite@gmail.com>
2022-01-13 12:16:57 +0100

    Fix king coordinates and exception

--- include/chessgame/Human.h
+++ include/chessgame/Human.h
@@ -40,6 +40,8 @@ namespace chessgame
         std::array<Coordinates, 2> think()
         {
             std::string from, to;
+            std::string color = this->pieceColor == chessgame::WHITE ? "white" : "black";
+            std::cout << "Please state your move (" << color << "): ";
             std::cin >> from >> to;
             return std::array<Coordinates, 2>{Coordinates(from), Coordinates(to)};
         }
