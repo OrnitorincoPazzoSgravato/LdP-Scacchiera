abcc9b87389e7d501d9abba540e616d62000e3a8
Enrico <enrico.cavinato.1@studenti.unipd.it>
2022-01-17 11:11:21 +0100

    added replay.h and replay.cpp

--- include/Replay.h
+++ include/Replay.h
@@ -1,12 +1,12 @@
 /**
  * @file Replay.h
  * @author Enrico Cavinato
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-12-27
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #ifndef REPLAY_H
@@ -15,26 +15,122 @@
 #include <iostream>
 #include <istream>
 #include <string>
+#include <ostream>
 
-#include "Chessboard.h"
+#include "./chessgame/Chessboard.h"
+#include "./chessgame/Utilities.h"
 
-namespace chessgame
+namespace replay_game
 {
-    
         /**
-         * @brief print a game on screen from a log file. The function prints the seuqence of the states of the chessboard before and avter every move
-         * 
-         * @param input_file the log file we want to read
-         * 
+         * @brief This class implement a replay from a log file.
+         *
+         * Each line of log file must contain, in this order and separater with " "
+         *
+         *      - Initial coordinates of a valid move in symbol form, ex. "A1" coordinates of a valid move in symbol form, ex. "A1"
+         *      - Final coordinate of a valid move in symbol form, ex. "A1"
+         *      - Flag char for promotion : it has to be the symbol of the promoted piece after the promotion
          */
-        void print_onscreen(const std::string &input_file);
+        class Replay
+        {
+        public:
+                /**
+                 * @brief Construct a new Replay object
+                 *
+                 * @param input a string with the input file
+                 * @param output file where we print output,if it is "screen" print on screen
+                 */
+                Replay(const std::string &input, const std::string &output);
+                /**
+                 * @brief print content of input file
+                 *
+                 */
+                void print();
+                /**
+                 * @brief Copy constructor disabled
+                 *
+                 */
+                Replay(const Replay &) = delete;
+                /**
+                 * @brief Destroy the Replay object
+                 *
+                 */
+                ~Replay();
+
+        private:
+                /**
+                 * @brief a Chessboard where we replay the game
+                 */
+                chessgame::Chessboard board;
+                /**
+                 * @brief the input file
+                 *
+                 */
+                std::fstream input_file;
+                /**
+                 * @brief the output file
+                 *
+                 */
+                std::fstream output_file;
+                /**
+                 * @brief is this a onscreen replay?
+                 *
+                 */
+                bool is_onscreen_replay{false};
+                /**
+                 * @brief make the move in a chessboard
+                 *
+                 */
+                std::array<chessgame::Coordinates, 2> move();
+                /**
+                 * @brief this function checks if this move is an arrocco and move the other piece ( tower or king)
+                 *
+                 * @param from initial cell of the move
+                 * @param to final cell of the move
+                 */
+                void arrocco_move(const chessgame::Coordinates from, const chessgame::Coordinates to);
+                /**
+                 * @brief check if the move is an enpassant capture and capture the opposite paw
+                 *
+                 * @param from initial cell of the move
+                 * @param to final cell of the move
+                 */
+                void en_passant_capture(const chessgame::Coordinates &from, const chessgame::Coordinates &to);
+                /**
+                 * @brief print on file
+                 *
+                 */
+                void print_on_file();
+                /**
+                 * @brief print on screen
+                 *
+                 */
+                void print_on_screen();
+        };
         /**
-         * @brief print a game on a log file from a log file. The function prints the seuqence of the states of the chessboard before and avter every move
-         * 
-         * @param input_file the log file we want to read
-         * 
+         * @brief Coordinate iniziali torre destra nera H8
          */
-        void print_on_file(const std::string &input_file, const std::string &output_file);
+        const chessgame::Coordinates T_destra{"H8"};
+        /**
+         * @brief Coordinate iniziali torre sinistra nera A8
+         */
+        const chessgame::Coordinates T_sinistra{"A8"};
+        /**
+         * @brief Coordinate iniziali torre destra bianca H1
+         */
+        const chessgame::Coordinates t_destra{"H1"};
+        /**
+         * @brief Coordinate iniziali torre sinistra bianca A1
+         */
+        const chessgame::Coordinates t_sinistra{"A1"};
+        /**
+         * @brief Coordinate iniziali RE NERO E8
+         */
+        const chessgame::Coordinates Re_nero_init{"E8"};
+        /**
+         * @brief Coordinate iniziali RE BIANCO E1
+         */
+        const chessgame::Coordinates re_bianco_init{"E1"};
 }
 
 #endif
--- lib/Replay.cpp
+++ lib/Replay.cpp
@@ -1,65 +1,200 @@
 /**
  * @file Replay.cpp
- * @author Riccardo Zuech
+ * @author Enrico Cavinato
  * @brief
  * @version 0.1
- * @date 2022-01-06
+ * @date 2022-01-16
  *
  * @copyright Copyright (c) 2022
  *
  */
 #include <string>
 #include <fstream>
-
+#include <ostream>
+#include <chrono>
+#include <thread>
 #include <stdexcept>
+#include <iostream>
+#include <istream>
 
 #include "../include/Replay.h"
+#include "Game.h"
 
-namespace chessgame
+namespace replay_game
 {
-    void print_onscreen(const std::string &input_file)
+    Replay::Replay(const std::string &input, const std::string &output)
     {
-        // opens the input file
-        std::ifstream ist{input_file};
-
-        // if the name is invalid
-        if (!ist)
+        input_file.open(input, std::fstream::in);
+        if (!input_file)
             throw std::invalid_argument("This file does not exist");
+        if (output == "screen")
+            is_onscreen_replay = true;
+        else
+            output_file.open(output, std::fstream::out);
+    }
 
-        // create a chessboard to recreate the moves
-        Chessboard board;
+    void Replay::print()
+    {
+        // if file not open
+        if (!input_file.is_open())
+            throw std::invalid_argument("File not open");
 
-        // print initial configuration
-        std::cout << board.snapshot();
+        // if replay on screen
+        if (is_onscreen_replay)     print_on_screen();
 
-        // while loop input
-        while (!ist.eof())
+        // else replay on file
+        else     print_on_file();
+
+        // close input file
+        input_file.close();
+    }
+
+    void Replay::print_on_file()
+    {
+        // check if output file is open
+        if (!output_file.is_open())
+            throw std::invalid_argument("File not open");
+        
+        output_file << "-----------REPLAY OF THE GAME-----------" << std::endl;
+
+        //while loop
+        while (!input_file.eof())
         {
-            Coordinates from;
-            Coordinates to;
-            // get coordinates in from
-            // get Coordinates in to
+            // print the state of chessboard
+            output_file << this->board.snapshot() << std::endl;
 
-            // update chessboard
-            Piece *p{board.get_piece(from)};
-            board.set_piece(to, p);
+            // make the move
+            std::array<chessgame::Coordinates, 2> this_move = move();
+            chessgame::Coordinates from{this_move[0]};
+            chessgame::Coordinates to{this_move[1]};
 
-            // print configuration
-            std::cout << board.snapshot();
+            output_file << "Moved: " << this->board.get_piece(to)->getSymbol() << " from " << from.symbol << " to " << to.symbol << std::endl;
+            output_file << "********************** End of turn **********************" << std::endl;
         }
-        ist.close();
+        output_file << "********************** GAME OVER **********************" << std::endl;
+        output_file.close();
     }
-    void print_on_file(const std::string &input_file, const std::string &output_file)
+
+    void Replay::print_on_screen()
     {
-        // opens the input file
-        std::ifstream ist{input_file};
+        std::cout << "-----------REPLAY OF THE GAME-----------" << std::endl;
+        // while loop input
+        while (!input_file.eof())
+        {
+            // print the state of chessboard
+            std::cout << this->board.snapshot() + "\n";
 
-        // if the name is invalid
-        if (!ist)
-            throw std::invalid_argument("This file does not exist");
+            // wait 1 second before the move
+            std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        // create a chessboard to recreate the moves
-        Chessboard board;
+            // make the move
+            std::array<chessgame::Coordinates, 2> this_move = move();
+            chessgame::Coordinates from{this_move[0]};
+            chessgame::Coordinates to{this_move[1]};
+
+            std::cout << "Moved: " << this->board.get_piece(to)->getSymbol() << " from " << from.symbol << " to " << to.symbol << std::endl;
+            std::cout << "********************** End of turn **********************" << std::endl;
+        }
+        std::cout << "********************** GAME OVER **********************" << std::endl;
     }
 
+    std::array<chessgame::Coordinates, 2> Replay::move()
+    {
+        // elements of the move:
+        std::string initial;
+        std::string final;
+        char promotion_char;
+
+        // get input
+        this->input_file >> initial >> final >> promotion_char;
+        chessgame::Coordinates from{initial};
+        chessgame::Coordinates to{final};
+
+        chessgame::check_coordinates(from);
+        chessgame::check_coordinates(to);
+        // if to is an empty cell
+        if (!this->board.get_piece(to))
+        {
+            // if this is an enpassant or arrocco move
+            en_passant_capture(from, to);
+            arrocco_move(from, to);
+
+            // move
+            board.swap_positions(from, to);
+        }
+        // if to is not an empty cell
+        else
+        {
+            board.set_piece(to, nullptr);
+            board.swap_positions(from, to);
+        }
+
+        // try promotion
+        this->board.promote(promotion_char, to);
+
+        return std::array<chessgame::Coordinates, 2>{from, to};
+    }
+
+    void Replay::arrocco_move(const chessgame::Coordinates from, const chessgame::Coordinates to)
+    {
+
+        // se non è arrocco termina
+        if (!gameplay::isArrocco(std::array<chessgame::Coordinates, 2>{from, to}, board))
+            return;
+
+        // se muovo il re
+        if (from == Re_nero_init || from == re_bianco_init)
+        {
+            // case 1 : nero a sinisra
+            if (to.symbol[0] < Re_nero_init.symbol[0])
+                this->board.swap_positions(T_sinistra, chessgame::Coordinates{"D8"});
+            // case 2 :nero a destra
+            else if (to.symbol[0] > Re_nero_init.symbol[0])
+                this->board.swap_positions(T_destra, chessgame::Coordinates{"F8"});
+
+            // case 3:bianco a sinistra
+            else if (to.symbol[0] < re_bianco_init.symbol[0])
+                this->board.swap_positions(t_sinistra, chessgame::Coordinates{"D1"});
+
+            // case 4 :bianco a destra
+            else if (to.symbol[0] > re_bianco_init.symbol[0])
+                this->board.swap_positions(chessgame::Coordinates{"F1"}, t_destra);
+            return;
+        }
+        // case 5 : muovo la torre nera di sinistra
+        else if (from == T_sinistra)
+            this->board.swap_positions(Re_nero_init, chessgame::Coordinates{"C8"});
+
+        // case 6 : muovo torre nera di destra
+        else if (from == T_destra)
+            this->board.swap_positions(Re_nero_init, chessgame::Coordinates{"G8"});
+        // case 7 : muovo torre bianca di sinistra
+        else if (from == t_sinistra)
+            this->board.swap_positions(re_bianco_init, chessgame::Coordinates{"C1"});
+
+        // case 8 :muovo torre bianca di destra
+        else if (from == t_destra)
+            this->board.swap_positions(re_bianco_init, chessgame::Coordinates{"G1"});
+    }
+
+    void Replay::en_passant_capture(const chessgame::Coordinates &from, const chessgame::Coordinates &to)
+    {
+        // if column is different but cell is empty
+        if (from.x != to.x && !this->board.get_piece(to))
+        {
+            chessgame::Coordinates enpassant_coord{to.x, from.y};
+            this->board.set_piece(enpassant_coord, nullptr);
+        }
+        // if not enpassant, this function does nothing
+        return;
+    }
+
+    Replay::~Replay()
+    {
+        //close io streams
+        if (this->input_file.is_open())
+            this->input_file.close();
+        if (this->output_file.is_open())
+            this->output_file.close();
+    }
 }
