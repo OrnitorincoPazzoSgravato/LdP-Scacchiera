0e4da60d74e918fae9cadd56d71af9190ae7b6ff
HeliosVelite <heliosvelite@gmail.com>
2022-01-21 09:44:12 +0100

    Fixed arrocco and en passant segmentation

--- lib/chessgame/Utilities.cpp
+++ lib/chessgame/Utilities.cpp
@@ -1,7 +1,18 @@
+/**
+ * @file Utilities.cpp
+ * @author
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "../../include/chessgame/Utilities.h"
 
 namespace chessgame
-{
+{   
+
     Coordinates::Coordinates(int x_coord, int y_coord) : x{x_coord}, y{y_coord}
     {
         /*if (x < 0 || x > 7 || y < 0 || y > 7)
